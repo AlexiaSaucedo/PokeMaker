b05722ca3d317694fa136e797c524ffa7d7baf15
AlexiaSaucedo <AlexiaSaucedo@users.noreply.github.com>
2024-01-18 18:33:22 -0600

    Model: Add ingredients array and randomizer button

--- PokeMaker-SwiftUI/ContentView.swift
+++ PokeMaker-SwiftUI/ContentView.swift
@@ -8,15 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var ingredient: Ingredient
+    
     var body: some View {
         VStack {
-            PokeCard()
+            PokeCard(ing: ingredient)
             Text("Poke Bowls")
                 .font(.largeTitle)
                 .bold()
             Text("Not sure which one?")
             Button("Give me some ingredients"){
-                
+                self.ingredient = ingredients.randomElement()!
             } .buttonStyle(.borderedProminent)
 
         }
@@ -25,27 +28,25 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ContentView(ingredient: Ingredient(id: "1", name: "Shrimp", category: "Protein"))
 }
 
 struct PokeCard: View {
+    
+    let ing : Ingredient
+    
     var body: some View {
+        
         ZStack {
             RoundedRectangle(cornerRadius: 10)
                 .foregroundStyle(Color(uiColor: UIColor(red: 0.97, green: 0.88, blue: 0.85, alpha: 1.00)))
                 .frame(height: 500)
                 .padding()
             VStack {
-                Text("PROTEIN: Shrimp")
+                Text("PROTEIN: \(ing.name)")
                     .bold()
                     .padding()
-                ZStack {
-                    Circle()
-                        .foregroundStyle(.white)
-                    Image("ing1")
-                        .resizable()
-                    .aspectRatio(1, contentMode: .fit)
-                }.frame(width: 150, height: 150)
+                IngredientView(n: ing.id)
                 Image("poke1")
                     .resizable()
                     .aspectRatio(1, contentMode: .fit)
@@ -54,3 +55,31 @@ struct PokeCard: View {
         }
     }
 }
+
+struct IngredientView: View {
+    let n: String
+    var body: some View {
+        
+        ZStack {
+            Circle()
+                .foregroundStyle(.white)
+            Image("ing\(n)")
+                .resizable()
+                .aspectRatio(1, contentMode: .fit)
+        }.frame(width: 150, height: 150)
+    }
+}
+
+//Continue with Networking
+
+struct Ingredient: Identifiable {
+    let id: String
+    let name: String
+    let category: String
+}
+
+let ingredients = [
+    Ingredient(id: "1", name: "Shrimp", category: "Protein"),
+    Ingredient(id: "2", name: "Salmon", category: "Protein"),
+    Ingredient(id: "3", name: "Chicken", category: "Protein")
+]
--- PokeMaker-SwiftUI/PokeMaker_SwiftUIApp.swift
+++ PokeMaker-SwiftUI/PokeMaker_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PokeMaker_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(ingredient: Ingredient(id: "1", name: "Shrimp", category: "Protein"))
         }
     }
 }
